82afb52f24ae5dc46524063a193adf166e13a7e3
Mateus Lira <111605369+MATTEUSxx@users.noreply.github.com>
2023-11-21 14:26:28 -0300

    Add files via upload

---
+++ P2/src/main/java/com/example/crudquestaoalternativa/CrudQuestaoAlternativaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudquestaoalternativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudQuestaoAlternativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudQuestaoAlternativaApplication.class, args);
+	}
+
+}
---
+++ P2/src/test/java/com/example/crudquestaoalternativa/CrudQuestaoAlternativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudquestaoalternativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudQuestaoAlternativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
